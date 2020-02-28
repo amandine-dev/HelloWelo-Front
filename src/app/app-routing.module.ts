448147dbb7b937853253b5c1ce7895c95348739b
BuffySummers1 <57723313+BuffySummers1@users.noreply.github.com>
2020-02-29 00:23:20 +0100

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LesSortiesComponent } from './pages/les-sorties/les-sorties.component';
+import { ProposerUneBaladeComponent } from './pages/proposer-une-balade/proposer-une-balade.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { MonEspaceComponent } from './pages/mon-espace/mon-espace.component';
+import { ConnexionComponent } from './pages/connexion/connexion.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  {
+    path: 'page',
+    children: [
+      { path:'les-sorties', component: LesSortiesComponent },
+      { path:'proposer-une-balade', component : ProposerUneBaladeComponent },
+      { path:'contact', component: ContactComponent },
+      { path:'mon-espace', component: MonEspaceComponent},
+      { path:'connexion', component: ConnexionComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
