7a8637a64dc89c0bc4056cfb3f4f58508e92788e
Lucie KOZIK <lucie.kozik@hotmail.fr>
2020-04-03 15:52:19 +0200

    premier jet ajout de participants

--- src/app/elements/bikeride-list/bikeride-list.component.ts
+++ src/app/elements/bikeride-list/bikeride-list.component.ts
@@ -15,7 +15,6 @@ export class BikerideListComponent implements OnInit {
   bikerides: BikerideModel[];
   participants: ParticipantModel[];
   numberMaxParticipants: any;
-  // numberParticipants: participants.length;
   participant: ParticipantModel;
 
   constructor(
@@ -33,18 +32,35 @@ export class BikerideListComponent implements OnInit {
       );
   }
 
-  // addParticipant() {
+  // addParticipant(participants: ParticipantModel[]) {
   //   alert("Vous êtes bien inscrits")
-  //   if (this.numberParticipants <= this.numberMaxParticipants) {
-  //     this.participant = ()
-  //     this.participantService.addParticipant()
+
+  //   if (this.participants.length <= this.numberMaxParticipants) {
+
+  //     // this.participant = (this.userService.getUser(id));
+
+  //     this.participantService.addParticipant(this.participant)
   //       .subscribe(
-  //         (data: ParticipantModel) => console.log('Participand ajouté')
+  //         (data: ParticipantModel[]) => this.bikerides = data
   //       );
 
   //   } else {
   //     alert("il n'y a plus de places")
   //   }
+  // }
+
+  // onClick(): void {
+  //     let numberparticipants = this.participants.length;
+
+  //     this.addbikerideService.saveBikeride(bikeride)
+  //       .subscribe(
+  //         (data: BikerideModel) => {
+  //           this.router.navigate(['/les-sorties']);
+  //         },
+  //         (err: Error) => console.log(err),
+  //         () => console.log('Request completed')
+  //       );
+  //     console.log(this.form.value);
   // }
   
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,11 @@
+import { ErrorhandlerService } from './error-handler.service';
+import { tap, catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UserModel } from '../models/user.models';
 import { Observable } from 'rxjs';
+
+const apiUrl = "http://localhost:3000/users";
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +13,24 @@ import { Observable } from 'rxjs';
 export class UserService {
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private errorHandlerService: ErrorhandlerService
   ) { }
 
   save(user: UserModel): Observable<UserModel> {
     return this.http.post<UserModel>('http://localhost:3000/users/signin', user);
   }
 
+  getUser(id: number): Observable<any> {
+    const url = `${apiUrl}/${id}`;
+
+    return this.http.get(url)
+      .pipe(
+        tap((user: UserModel) => console.log(`fetched participant id =${id}`)),
+
+        catchError(this.errorHandlerService.handleError<UserModel>('getUser'))
+      );
+  }
 }
 
 
