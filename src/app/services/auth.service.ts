f894c1c21e1273f1cb164c8617fd1983a141c886
Lucie KOZIK <lucie.kozik@hotmail.fr>
2020-04-06 10:33:15 +0200

    fix localStorage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,61 +4,50 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { UserModel } from '../models/user.models';
 
-
 interface UserAuth {
   message: string;
-  user: UserModel;
+  User: UserModel;
   token: string;
 }
-
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private userSubject: BehaviorSubject<UserModel>;
-  private user$: Observable<UserModel>
+  user$: Observable<UserModel>
   private loginUrl = 'http://localhost:3000/users/login';
-
   constructor(private http: HttpClient) {
     this.userSubject = new BehaviorSubject<UserModel>(null);
     const data = localStorage.getItem('user_storage');
-
     if (data !== "undefined") {
       const user = JSON.parse(data);
-
       this.userSubject.next(user);
       this.user$ = this.userSubject.asObservable();
     }
   }
-
   // this.authService.user
   // value === UserModel | null
   public get user(): UserModel {
     return this.userSubject.value;
   }
-
   public getToken() {
     return localStorage.getItem('user_token');
   }
-
   public login(email: string, password: string): Observable<UserModel> {
     const value = { email: email, password: password };
-
     return this.http.post<UserAuth>(this.loginUrl, value)
       .pipe(
         tap((data: UserAuth) => console.log(data))
       )
       .pipe(
         map((data: UserAuth) => {
-          localStorage.setItem('user_storage', JSON.stringify(data.user));
+          localStorage.setItem('user_storage', JSON.stringify(data.User));
           localStorage.setItem('user_token', JSON.stringify(data.token));
-          this.userSubject.next(data.user);
-
-          return data.user;
+          this.userSubject.next(data.User);
+          return data.User;
         })
       );
   }
-
   logout(): void {
     localStorage.removeItem('user_storage');
     localStorage.removeItem('user_token');
