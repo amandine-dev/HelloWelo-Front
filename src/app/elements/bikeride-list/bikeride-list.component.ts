c8cb997f0b2ee8cb1ce9e50e7da833ecbc44ad12
Lucie KOZIK <lucie.kozik@hotmail.fr>
2020-04-07 15:54:24 +0200

    ajout des modifs pour le bouton jy go

--- src/app/elements/bikeride-list/bikeride-list.component.ts
+++ src/app/elements/bikeride-list/bikeride-list.component.ts
@@ -1,13 +1,11 @@
-import { ParticipantModel } from './../../models/participant.model';
-import { UserService } from 'src/app/services/user.service';
-import { ParticipantService } from './../../services/participant.service';
 import { Router } from '@angular/router';
 import { AddbikerideService } from 'src/app/services/addbikeride.service';
 import { BikerideModel } from '../../models/bikeride.models';
 import { Component, OnInit } from '@angular/core';
 import { UserModel } from 'src/app/models/user.models';
 import { AuthService } from 'src/app/services/auth.service';
-
+import { ParticipantModel } from 'src/app/models/participant.model';
+import { ParticipantService } from 'src/app/services/participant.service';
 @Component({
   selector: 'app-bikeride-list',
   templateUrl: './bikeride-list.component.html',
@@ -15,28 +13,22 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class BikerideListComponent implements OnInit {
   // list of bike rides
-  bikerides: any=[];
-  participant: ParticipantModel;
-
+  bikerides: BikerideModel[];
   // bike ride selected by the participant
   selectedBikeRide: BikerideModel;
   bikeRideId: number;
   numberMaxParticipants: number;
   numberParticipants: number;
-
   // user id
   UserId: number;
-
   // new participant to be added to bike ride participants list
   newParticipant: ParticipantModel = new ParticipantModel();
-
   constructor(
     private addbikerideService: AddbikerideService,
     private authService: AuthService,
     private router: Router,
     private participantService: ParticipantService
   ) { }
-
 
   ngOnInit(): void {
     this.addbikerideService.getBikerides()
@@ -46,32 +38,27 @@ export class BikerideListComponent implements OnInit {
           console.log(this.bikerides);
         }
       );
+    this.UserId = this.authService.user.id;
+    console.log(this.UserId);
   }
-
   setSelectedBikeRide(selectedBikeRide: BikerideModel) {
     this.selectedBikeRide = selectedBikeRide;
     console.log(selectedBikeRide);
-
     this.bikeRideId = selectedBikeRide.id;
     console.log(this.bikeRideId);
-
     this.numberParticipants = selectedBikeRide.numberParticipants;
     console.log(this.numberParticipants);
-
     this.numberMaxParticipants = selectedBikeRide.numberMaxParticipants;
     console.log(this.numberMaxParticipants);
-
     this.setNewParticipant(this.bikeRideId, this.UserId);
     this.addParticipant(this.newParticipant);
   }
-
 
   setNewParticipant(bikRideId, userId) {
     this.newParticipant.BikeRideId = bikRideId;
     this.newParticipant.UserId = userId;
     console.log(this.newParticipant);
   }
-
   addParticipant(participant: ParticipantModel) {
     if (this.numberParticipants <= this.numberMaxParticipants) {
       this.participantService.addParticipant(participant)
@@ -86,5 +73,4 @@ export class BikerideListComponent implements OnInit {
       console.log("Full bike ride");
     }
   }
-
 }
